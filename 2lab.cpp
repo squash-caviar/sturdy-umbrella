5a84706b4a067f4d5987311ac7fb8e74b74ae1d0
cocairo <44133122+cocairo@users.noreply.github.com>
2018-11-03 23:58:34 +0300

    Update 2lab.cpp

--- 2lab.cpp
+++ 2lab.cpp
@@ -1,88 +1,89 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Book
 {
 
-public:
-
-    Book(string, string, int)
-    {
-
-        namebook = "unknown";
-        creator = "unknown";
-        year = 404;
-
-    };
-
-    ~Book();
-
-    void setnamebook(string temp)
-    {
-
-        namebook = temp;
-
-    };
-
-    void setcreator(string temp)
-    {
-
-        creator = temp;
-
-    };
-
-    void setyear(int temp)
-    {
-
-        year = temp;
-
-    };
-
-    string getnamebook()
-    {
-
-        return namebook;
-
-    };
-
-    string getcreator()
-    {
-
-        return creator;
-
-    };
-
-    int getyear()
-    {
-
-        return year;
-
-    };
-
 private:
 
     string namebook;
     string creator;
     int year;
+
+public:
+
+    Book ();
+
+    Book (string, string, int);;
+
+
+
+    void setnamebook (string temp);;
+
+    void setcreator(string temp);;
+
+    void setyear(int temp);;
+
+    string getnamebook();;
+
+    string getcreator();;
+
+    int getyear();;
+
+
 };
 
+Book::Book() {
+
+}
+
+Book::Book(string, string, int) {
+    namebook = "unknown";
+    creator = "unknown";
+    year = 404;
+}
+
+void Book::setnamebook(string temp) {
+    namebook = temp;
+}
+
+void Book::setcreator(string temp) {
+    creator = temp;
+}
+
+void Book::setyear(int temp) {
+    year = temp;
+}
+
+string Book::getnamebook() {
+    return namebook;
+}
+
+string Book::getcreator() {
+    return creator;
+}
+
+int Book::getyear() {
+    return year;
+}
 
 
 int main()
 {
 
-    Book b1();
+    Book b1;
     Book b2("The Engineers and the Price System" , "Thorstein Bunde Veblen" , 1921);
     Book b3("The Theory of Leisure Class" , "Thorstein Bunde Veblen" , 1899);
 
     b1.setnamebook("Harry Potter and the Methods of Rationality");
     b1.setcreator("Eliezer S. Yudkowsky");
-    b1.setyear("2015")
+    b1.setyear(2015);
 
     cout << b1.getnamebook() << "   " << b1.getcreator() << b1.getyear() << endl;
     cout << b2.getnamebook() << "   " << b2.getcreator() << b2.getyear() << endl;
-    cout << b3.getnamebook() << "   " << b3.getcreator() << b3.getyear() << endl;
+    cout << b3.getyear() << "   " << b3.getcreator() << b3.getyear() << endl;
 
     Book lib[3];
 
@@ -92,17 +93,17 @@ int main()
     for (int i = 1; i<4; i++)
     {
 
-    cout << "enter name book ";
-    cin >> sbuf;
-    lib[i].setnamebook(sduf);
+        cout << "enter name book ";
+        cin >> sbuf;
+        lib[i].setnamebook(sbuf);
 
-    cout << "enter name creator ";
-    cin >> sbuf;
-    lib[i].setcreator(sduf);
+        cout << "enter name creator ";
+        cin >> sbuf;
+        lib[i].setcreator(sbuf);
 
-    cout << "enter year of writing ";
-    cin >> ybuf;
-    lib[i].setyear(yduf);
+        cout << "enter year of writing ";
+        cin >> ybuf;
+        lib[i].setyear(ybuf);
 
     }
 
@@ -110,7 +111,7 @@ int main()
     for (int i = 1; i<4; i++)
     {
 
-    cout << lib[i].getnamebook() << "  " << lib.getcreator() << "  " << lib.getyear() << endl;
+        cout << lib[i].getnamebook() << "  " << lib[i].getcreator() << "  " << lib[i].getyear() << endl;
 
     }
 
