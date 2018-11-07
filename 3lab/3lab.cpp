82da156ab38635f8a8223b7199cdb75d0c5ed6c2
cocairo <cocairo@ya.ru>
2018-11-07 20:25:03 +0300

    Сделяль

--- 3lab/3lab.cpp
+++ 3lab/3lab.cpp
@@ -6,14 +6,87 @@ using namespace std;
 
 main()
 {
-    Mobile_phone Nokia_3310;
-    Nokia_3310.Power_on();
-    Nokia_3310.Network_search();
-    Nokia_3310.Printinfo();
+	Mobile_phone Nokia_3310 ("Nokia", "3310");
+	Nokia_3310.Power_on();
+	Nokia_3310.Network_search();
+	Nokia_3310.Printinfo();
 
 
-	
+	int j = 1;
 
-    return 0;
+	while (j)
+	{
+
+		cout << "Menu"<< endl;
+		cout << "1. Info"<< endl;
+		cout << "2. Power on"<< endl;
+		cout << "3. Power off"<< endl;
+		cout << "4. Flashlight_on"<< endl;
+		cout << "5. Flashlight_off"<< endl;
+		cout << "6. Volume_up"<< endl;
+		cout << "7. Volume_off"<< endl;
+		cout << "8. Network_search"<< endl;
+		cout << "9. Exit"<< endl;
+		cout << "enter a number to continue " << endl;
+
+		int i = 0;
+
+		cin >> i;
+
+		cout << endl;
+
+		switch ( i ) {
+
+			case 1: 
+			
+				Nokia_3310.Printinfo();
+				break;
+			
+			
+			case 2:
+			
+				Nokia_3310.Power_on();
+				break;
+			
+
+			case 3: 
+			
+				Nokia_3310.Power_off();
+				break;
+			
+
+			case 4:
+			
+				Nokia_3310.Flashlight_on();
+				break;
+
+
+			case 5:
+			
+				Nokia_3310.Flashlight_off();
+				break;
+			
+
+			case 6:
+
+				Nokia_3310.Network_search();
+				break;
+
+			case 7:
+
+				j =0;
+				break;
+
+			default:
+			{
+				cout << "doh"<< endl;
+				cout << endl;
+				break;
+			}
+
+		}
+	}
+
+	return 0;
 }
 
--- 3lab/class.h
+++ 3lab/class.h
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-//#include <ctime>
 
 using namespace std;
 
@@ -8,63 +7,71 @@ class Mobile_phone
 {
 private:
 
-    bool Power_supply;
-    bool Frash_light;
-    short int Signal_level;
-    long int IMEI;
-    string Model;
-    string Brand;
+	bool Power_supply;
+	bool Frash_light;
+	short int Volume;
+	short int Signal_level;
+	long int IMEI;
+	string Model;
+	string Brand;
 
 public:
-    Mobile_phone();;
+	Mobile_phone();;
 
-    Mobile_phone(string m, string b);;
+	Mobile_phone(string m, string b);;
 
-    ~Mobile_phone();;
+	~Mobile_phone();;
 
-    void Power_on();;
+	void Power_on();;
 
-    void Power_off();;
+	void Power_off();;
 
-    void Flash_on();;
+	void Flashlight_on();;
 
-    void Flash_off();;
+	void Flashlight_off();;
 
-    void Network_search();;
+	void Network_search();;
 
-    void Printinfo();;
+	void Printinfo();;
+
+	void Volume_up();;
+
+	void Volume_down();;
 
 };
 
 Mobile_phone::Mobile_phone()
 {
-    Power_supply = 0;
-    Frash_light = 0;
-    Signal_level = 0;
+	Power_supply = 0;
+	Frash_light = 0;
+	Signal_level = 0;
+	Volume = 0;
 
-    Model = "unknown";
-    Brand = "unknown";
+	Model = "unknown";
+	Brand = "unknown";
 
-    long int temp;
-    srand ( time(NULL) );
-    temp = rand() % 2147483647 + 1000000000;
-    
-    if (temp > 0)
-        IMEI = temp;
-        else 
-            IMEI = temp * -1;
+	long int temp;
+	srand ( time(NULL) );
+	temp = rand() % 2147483647 + 1000000000;
+	
+	if (temp > 0)
+		IMEI = temp;
+		else 
+			IMEI = temp * -1;
 }
 
 Mobile_phone::Mobile_phone(string m, string b)
 {
-    Power_supply = 0;
-    Frash_light = 0;
-    Signal_level = 0;
-    
-    srand ( time(NULL) ); 
-    IMEI = rand() % 5;
-    Model = m;
-    Brand = b;
+	Power_supply = 0;
+	Frash_light = 0;
+	Signal_level = 0;
+	Volume = 0;
+	
+	srand ( time(NULL) ); 
+	IMEI = rand() % 5;
+
+	Model = m;
+	Brand = b;
 }
 
 Mobile_phone::~Mobile_phone()
@@ -73,43 +80,60 @@ Mobile_phone::~Mobile_phone()
 
 void Mobile_phone::Power_on ()
 {
-    Power_supply = 1;
-    Network_search();
+	Power_supply = 1;
+	Network_search();
 }
 
 void Mobile_phone::Power_off()
 {
-    Power_supply = 0;
-    Frash_light = 0;
-    Signal_level = 0;
+	Power_supply = 0;
+	Frash_light = 0;
+	Signal_level = 0;
 }
 
-void Mobile_phone::Flash_on()
+void Mobile_phone::Flashlight_on()
 {
-    if (Power_supply) 
-        Frash_light = 1;
+	if (Power_supply) 
+		Frash_light = 1;
 }
 
-void Mobile_phone::Flash_off()
+void Mobile_phone::Flashlight_off()
 {
-    Frash_light = 0;
+	Frash_light = 0;
 }
 
 void Mobile_phone::Network_search()
 {
-    if (Power_supply)
-        srand ( time(NULL) ); 
-        Signal_level = rand() % 5;
+	if (Power_supply)
+		srand ( time(NULL) ); 
+		Signal_level = rand() % 5;
+}
+
+void Mobile_phone::Volume_up ()
+{
+
+	if ((Volume < 100) && (Power_supply = 1))
+		Volume = Volume + 10;
+
+}
+
+void Mobile_phone::Volume_down ()
+{
+
+	if ((Volume > 0 ) && (Power_supply = 1))
+		Volume = Volume - 10;
+
 }
 
 void Mobile_phone::Printinfo()
 {
-    
-    cout << "Brand: " << Brand << endl; 
-    cout << "Model: " << Model << endl; 
-    cout << "IMEI: " << IMEI << endl;
-    cout << "Phone included: " << Power_supply << endl;
-    cout << "Signal level: " << Signal_level << endl; 
-    cout << "Flashlight included: " << Frash_light << endl;
-     
+	
+	cout << "Brand: " << Brand << endl; 
+	cout << "Model: " << Model << endl; 
+	cout << "IMEI: " << IMEI << endl;
+	cout << "Phone included: " << Power_supply << endl;
+	cout << "Signal level: " << Signal_level << endl; 
+	cout << "Volume: " << Volume << endl; 
+	cout << "Flashlight included: " << Frash_light << endl << endl;
+	 
 }
